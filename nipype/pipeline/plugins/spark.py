75d5737c6f96be02d04c1061f6cbd8a8e86d148c
shariqiqbal2810 <shariqiqbal2810@users.noreply.github.com>
2015-12-15 17:12:51 -0500

    refined skeleton for spark plugin

--- nipype/pipeline/plugins/spark.py
+++ nipype/pipeline/plugins/spark.py
@@ -1,7 +1,28 @@
-from .base import GraphPluginBase
+"""Parallel workflow execution via Spark
+"""
+import os
+import sys
+from .base import (PluginBase, logger)
+from ...interfaces.base import CommandLine
+from pyspark import SparkContext
+sc = SparkContext()
 
 
-class SGEGraphPlugin(GraphPluginBase):
+class SparkPlugin(PluginBase):
+
+    """Execute using Apache Spark
+
+    The plugin_args input to run can be used to control the execution.
+    Currently supported options are:
+    -ADD OPTIONS HERE
+
+    """
 
     def __init__(self, **kwargs):
-        super(SGEGraphPlugin, self).__init__(**kwargs)
+        super(SparkPlugin, self).__init__(**kwargs)
+
+    def run(self, flatgraph, execgraph, config):
+        return
+
+    def _submit_spark(self, pyfiles, execgraph, flatnodes):
+        return
