0cece18445663ca665f95f2081a9abe9c74c68ad
Bill Broderick <billbrod@gmail.com>
2015-12-15 19:04:31 -0500

    Adds sketch for _submit_spark, drafts for run and execNode, skeletons for execMapNode, execJoinNode, execIterables
    In order to get started here, I've put together a draft for whta I
    think run and execNode should do, as well as a sketch for how
    _submit_spark will run and function definitions for the other
    necessary functions (though they don't contain anything).
    
    run: takes flatgraph, execgraph, config, and updatehash from workflow,
    sets _config and creates an OrderedDict where keys are the flat graph
    nodes (in topological order) and values are the associated py
    files. Those pyfiles are created at this time (as are the associated
    pkl files for the nodes). This OrderedDict and the execgraph are then
    passed to _submit_spark.
    
    _submit_spark: this doesn't work as is, but I wanted to get ideas
    down. It will need to parallelize the initial pyfiles and broadcast
    the execgraph. It then iterate through the nodes in the OrderedDict,
    making sure the RDD contents are what we assume them to me (the
    corresponding pyfiles) and perform the equivalent operation (based on
    the type of node).
    
    execNode: this simply lodas in the pyfile so we can find the node,
    exec the pyfile, and return the successors of the node, using the
    broadcasted execgraph.

--- nipype/pipeline/plugins/spark.py
+++ nipype/pipeline/plugins/spark.py
@@ -1,10 +1,13 @@
 """Parallel workflow execution via Spark
 """
-import os
-import sys
-from .base import (PluginBase, logger)
+import sys,re,os
+from .base import (PluginBase, logger,create_pyscript)
+from nipype.pipeline.engine import JoinNode, MapNode, Node
+from nipype.utils.filemanip import loadpkl
 from ...interfaces.base import CommandLine
 from pyspark import SparkContext
+import networkx as nx
+from collections import OrderedDict
 sc = SparkContext()
 
 
@@ -21,8 +24,46 @@ class SparkPlugin(PluginBase):
     def __init__(self, **kwargs):
         super(SparkPlugin, self).__init__(**kwargs)
 
-    def run(self, flatgraph, execgraph, config):
+    def run(self, flatgraph, execgraph, config,updatehash=False):
+        self._config = config
+        logger.debug('Creating executable python files for each node')
+        nodes = OrderedDict()
+        for node in nx.topological_sort(flatgraph):
+            execnodes = [i for i in execgraph.nodes() if node.name==i.name]
+            pyfiles = [create_pyscript(i) for i in execnodes]
+            nodes[node] = pyfiles
+        _submit_spark(nodes,execgraph)
+
+    def _submit_spark(self, nodes, execgraph):
+        rdd = sc.parallelize(nodes.values[0])
+        broadcast_graph = sc.broadcast(execgraph)
+        for node in nodes:
+            #Make sure contents of RDD are what we think they are by checking against nodes[node]
+
+            #This is just a sketch of this...
+            if node.iterables:
+                rdd = rdd.flatMap(execIterables)
+            elif isinstance(node,JoinNode):
+                rdd = rdd.reduce(execJoinNode)
+            elif isinstance(node,MapNode):
+                rdd = rdd.flatMap(execMapNode)
+            else:
+                rdd = rdd.map(execNode)
+
+    def execNode(pyfile):
+        pyscript = open(pyfile).read()
+        pkl_file = re.findall(r"pklfile = '(.*pklz)'",pyscript)[0]
+        info = loadpkl(pkl_file)
+        exec(pyscript)
+        return broadcast_graph.value.successors(info['node'])
+
+    def execJoinNode(pyfile):
         return
 
-    def _submit_spark(self, pyfiles, execgraph, flatnodes):
+    def execMapNode(pyfile):
         return
+
+    def execIterables(pyfile):
+        return
+    
+    
